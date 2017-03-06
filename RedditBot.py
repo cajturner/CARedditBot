fc00ea847e8f130ef3f9c56e47fe2ff3203e1d79
calumturner <calum.turner@skyscanner.net>
2017-03-06 14:36:33 +0000

    Add telegram bot

---
+++ RedditBot.py
@@ -0,0 +1,28 @@
+import telegram
+import requests
+
+update_id = None
+
+
+def run():
+    global update_id
+    bot = telegram.Bot('360545018:AAGQ9Gz0sLriOYk0cAXOuh72n9UTmvl52Ks')
+    try:
+        update_id = bot.getUpdates()[0].update_id
+    except IndexError:
+        update_id = None
+
+    while True:
+        for update in bot.getUpdates(offset=update_id, timeout=10):
+            update_id = update.update_id + 1
+            if update.message:
+                try:
+                    r = requests.get('http://ec2-54-211-74-133.compute-1.amazonaws.com:5000',
+                                     params={"q": update.message.text})
+                    resp_value = r.json()[0]['value'][0]
+                    update.message.reply_text(resp_value)
+                except:
+                    pass
+
+if __name__ == '__main__':
+    run()
