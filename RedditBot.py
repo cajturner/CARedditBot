e58523d1988180064a49ff3eb954da88336bfd51
calumturner <calum.turner@skyscanner.net>
2017-04-04 11:28:30 +0100

    Alice bot

--- RedditBot.py
+++ RedditBot.py
@@ -6,7 +6,7 @@ update_id = None
 
 def run():
     global update_id
-    bot = telegram.Bot('TOKEN')
+    bot = telegram.Bot('354930862:AAFoCXTgomrLfvCsvp4a7oOcZjWrm54pXlw')
     try:
         update_id = bot.getUpdates()[0].update_id
     except IndexError:
@@ -17,9 +17,9 @@ def run():
             update_id = update.update_id + 1
             if update.message:
                 try:
-                    r = requests.get('http://ec2-54-211-74-133.compute-1.amazonaws.com:5000',
-                                     params={"q": update.message.text})
-                    resp_value = r.json()[0]['value'][0]
+                    r = requests.post('https://aiaas.pandorabots.com/talk/1409614304594/alice',
+                                     params={"user_key": "c70f5f260f515eb026675097239c19c9", "input": update.message.text})
+                    resp_value = r.json()['responses'][0]
                     update.message.reply_text(resp_value)
                 except:
                     pass
