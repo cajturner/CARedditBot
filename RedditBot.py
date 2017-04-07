6e30f4fc65f3e356d357f82bdb87e91983aa6180
calumturner <calum.turner@skyscanner.net>
2017-04-07 22:34:45 +0100

    Reddit bot with logging start

--- RedditBot.py
+++ RedditBot.py
@@ -1,14 +1,32 @@
-import telegram
-import requests
+from io import StringIO
+
 import boto3
+import datetime
+import requests
+import telegram
 
 update_id = None
+bot_name = ""
+convs = {}
 
 
 def upload_to_s3(data, key_name):
+    global bot_name
     s3 = boto3.resource('s3')
-    bucket = s3.Bucket('results')
-    bucket.upload_fileobj(data, key_name)
+    s = StringIO()
+    s.write(str("\n".join(data)))
+    s3.Object('redditbot', key_name + "_" + str(datetime.datetime.now()) + "_" + bot_name + ".txt").put(
+        Body=s.getvalue())
+    print("Exported to s3")
+
+
+def add_to_log(username, request, response):
+    global convs
+    try:
+        convs[username].append(request)
+        convs[username].append(response)
+    except KeyError:
+        convs[username] = [request, response]
 
 
 def run():
@@ -28,11 +46,27 @@ def run():
                                      params={"user_key": "c70f5f260f515eb026675097239c19c9", "input": update.message.text})
                     resp_value = r.json()['responses'][0]
                     update.message.reply_text(resp_value)
+                    username = update.message.from_user.username
+                    if update.message.text != "EXIT":
 
-                    # Log conv
+                        r = requests.get('http://ec2-54-211-74-133.compute-1.amazonaws.com:5000',
+                                         params={"q": update.message.text})
+                        resp_value = r.json()[0]['value'][0]
+                        update.message.reply_text(resp_value)
+                        log_request = str(update.message.date) + "    " + update.message.text
+                        log_response = str(datetime.datetime.now().replace(microsecond=0)) + "    " + resp_value
+                        add_to_log(username, log_request, log_response)
+                    else:  # Log conv
+                        resp_value = "Thanks for chatting to me! Goodbye."
+                        update.message.reply_text(resp_value)
+                        log_request = str(update.message.date) + "    " + update.message.text
+                        log_response = str(datetime.datetime.now().replace(microsecond=0)) + "    " + resp_value
+                        add_to_log(username, log_request, log_response)
+                        upload_to_s3(convs[username], username)
+                        convs.pop(username)
+                except Exception as e:
+                    print(e)
 
-                except:
-                    pass
 
 if __name__ == '__main__':
     run()
